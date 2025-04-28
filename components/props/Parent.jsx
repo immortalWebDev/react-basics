a62830f0f1b0af85484f45b9cd075add02d73e69
immortalWebDev <pgbadgujar007@gmail.com>
2025-04-28 23:54:46 +0530

    Add comments explaining prop drill

--- components/props/Parent.jsx
+++ components/props/Parent.jsx
@@ -1,7 +1,22 @@
 import Child from "./Child";
 
 const Parent = () => {
-  return <Child name="Piyush" job="WebDev"/>;
+  return <Child name="Piyush" job="WebDev" />;
 };
 
 export default Parent;
+
+/*
+Here the prop data is being sent from parent => child  => grandchild => greatgrandchild
+
+though we only need to sent data from parent to greatgrandchild
+
+thats bad that middle 2 components unnncessary had to carry the data props, its called prop drill
+
+and we cannot directly pass parent => greatgrandchild skipping the 2 middle components, though its technically possible
+
+react will not throw errors for that but it will break the component hierarchy and that will break the structure of whole
+app and distort whole UI
+
+
+*/
